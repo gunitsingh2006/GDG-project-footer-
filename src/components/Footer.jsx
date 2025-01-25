828825c62d4b13af59d1fc62abe9fd22f29faf1d
Gunit Singh <gunitsingh.duggal@gmail.com>
2025-01-25 17:57:02 +0530

    added icons and linked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,37 +3,64 @@ import "./style.css";
 
 const App = () => {
   return (
-    <div className="app2 min-h-screen">
-      {/* Main Container */}
-     
-        {/* Circle */}
-       
+    <React.Fragment>
+      <div className="app2 min-h-screen">
+        {/* Main Container */}
 
-        
-<div className="flex items-center ">
-       
-        <div className="circle2"></div> 
-       
-        <div className="heading2"><h3>  No sleep, all caffeine, and bugs are plot twists! <br />  Break, build, repeat and rule tech!
-        </h3></div>
+        {/* Circle */}
+
+        <div className="flex items-center ">
+          <div className="circle2"></div>
+
+          <div className="heading2">
+            <h3>
+              {" "}
+              No sleep, all caffeine, and bugs are plot twists! <br /> Break,
+              build, repeat and rule tech!
+            </h3>
+          </div>
         </div>
-     
-        
 
         <footer className="footer2 mt-96 ">
-        <div className="flex my-20 justify-center  space-x-6 mb-6 text-blue-600">
-         <img src="src/components/Frame 31.png" alt="" />
-          {/* <a href="#" className="text-2xl hover:text-blue-800" 
-          ></a>
-          <a href="#" className="text-2xl hover:text-blue-800" aria-label="LinkedIn">🔗</a>
-          <a href="#" className="text-2xl hover:text-blue-800" aria-label="Email">📧</a>
-          <a href="#" className="text-2xl hover:text-blue-800" aria-label="GitHub">🐙</a> */}
-        </div>
-        
-        <p>Made with <span role="img" aria-label="love">❤️</span> by GDG-GTBIT</p>
-        <p>Copyright &copy;2025, All rights reserved.</p>
-      </footer>
-    </div>
+          <div className="flex my-20 justify-center  space-x-6 mb-6 text-blue-600">
+            <a href="https://www.instagram.com">
+              <img
+                className="social"
+                src="https://s3-alpha-sig.figma.com/img/da62/3df0/221483b4afbc2c7bfe87c3f21262c2b1?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UwRADuu~Aotw25Kc6eS57rKcHeIiPXg-LZywZgqMKTzrk03DzW0u-RLZJtZyHeeoVwbvgRfMB7~l4Zz2gPqGuZTFAXGb2DvaqFnIW8-jTrsaR2Ft0Vl2mffErbpvacNlMhlO87Y4axUWWA9aAqZq~wXQyHdboHSR08TYahUk7pJmo4Mp-o4R8oucKHjUvFYyK~tZPWBAEOolvclvPVUnjpJ47ZCq3iradaUeiSS0MvrfTvJxRJqx7IeseXlcura8nIiPtGT9oYCKWFQkgr1jqp3DLy~jRAi2hjE~w48uwcvQU4hbBmJ-w3JJKyRxqiqlWA4WMMgJpU3rUru7dT0A1A__"
+               
+              />
+            </a>
+            <a href="https://www.instagram.com">
+              <img
+                className="social"
+                src="https://s3-alpha-sig.figma.com/img/49a7/0aa7/c67706ed0b4b4c05a73755a76decdba1?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qO74KLJ3KEjniZdbTotC9I8-fUPT2PGJWs48PhSdE8vFcR4PWp24lREIANxjMZbu0T4lvl2FF1NwRbgytRysbN7n94BxM8EfggQhSwEcTGKKwukgvmiFRaQ5Q1rvezXAeWZ-0OcXW4oWD1dHEOmcdm9g0-9qd6C68pohISCmrwk1F178c2EhAQB-6VcLEqIywkxa62Jpf-3O0s87~ufp2Hz~onncrXUzo1bdluwQWted8I3YqzgTaAgmsUT0tKnr0~ZHsV2s0mAHB~3UoR5cXorxYXqRHg46hnlqiPa2YGW5-vrkZLO7VehQZpG~sQ4EUbb2KlVzUDgTE3qXAow03w__"
+              />
+            </a>
+            <a href="https://www.instagram.com">
+              <img
+                className="social"
+                src="https://s3-alpha-sig.figma.com/img/d85f/7d9c/8e11cce81b39d00f15df03b7e89ee6e9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UteIo03nRA6cG7WadYFWCowcuYNa6P7-SAQnN~W5FXdV5KVgdyeMp54MKQqx-aBQIbj28nHlTV6bmzKDaNvnAD5F1sWSKHX80jSoa2shllF0sjE6bNYnD74D6aze1RJbAT9er1EoDlAh-KywV~qLsWJ96sP0SSBTjn5qRtpa~85po2aAARbDDXdYVwSZf4ieZV4g2gMzOdYNv~kopd35VDrkCAQ72ms7A1uoqzZcnjoYUfPRgyTPhl01dRO65Ia8gfLDk8rCIg3hpLoNWmktmfynHmdHufHMU0JapDk5akh-eQaDDKQ1gbOKhAJ1OJgazuvGG5KH8-GZoHCiEJyesA__"
+              />
+            </a>
+            <a href="https://www.instagram.com">
+              <img
+                className="social"
+                src="https://s3-alpha-sig.figma.com/img/9bd7/9f3e/1d9901d56ed7cfe7e01e6ae12db367bb?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BgNS93srCNU7vP59jcLbh6TTZOdY94pjBSiM6jfg9w1R4lFDtKCwdMWqUSwHy65KXsh1qTOSrmo~J8ybeEx1m9itsTwACpuoM0IxE13okw4Gf~GGXlCwqmECTkqkCu1mucuejAwvmClZEWxn9SMk5gPYir~PDiNtiJ7CYfuIw~lCuMCjdg~A5I1cJw-lYZhmG9t0hj79Xv7kA~YBm5z5sfKcCZDB53b9gmyonBmntLdeaCzyMhcSO3SPwZuDKjYjkMl9hxxAFaXueCoYCoo2kCwkrjLTNL4hGMttEVXxRvapexKuIDk-lcyxoLtI0kpO3DfogRkuP5LHboIZWjA8Sg__"
+              />
+            </a>
+          </div>
+
+          <p>
+            Made with{" "}
+            <span role="img" aria-label="love">
+              ❤️
+            </span>{" "}
+            by GDG-GTBIT
+          </p>
+          <p>Copyright &copy;2025, All rights reserved.</p>
+        </footer>
+      </div>
+    </React.Fragment>
   );
 };
 
