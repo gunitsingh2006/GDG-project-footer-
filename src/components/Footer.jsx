7809de1fd7635e08621ae34a9e25b0747d8989e8
Gunit Singh <gunitsingh.duggal@gmail.com>
2025-01-27 00:50:58 +0530

    converted css in inline

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,42 @@ import "./style.css";
 const App = () => {
   return (
     <React.Fragment>
-      <div className="app2 min-h-screen">
-        {/* Main Container */}
-
-        {/* Circle */}
-
+      <div
+        className="app2 px-16 py-20 h-72 "
+        style={{
+          fontFamily: "Pixelify Sans",
+          textAlign: "center",
+          color: "#090909",
+          left: "0%",
+          right: "0%",
+          background: "#C7DCFE",
+          borderRadius: "30px",
+        }}
+      >
         <div className="flex items-center ">
-          <div className="circle2"></div>
+          <div
+            className="circle2"
+            style={{
+              width: "250px",
+              height: "250px",
+              backgroundColor: "#d1d5db",
+              borderRadius: "50%",
+              alignItems: "center",
+            }}
+          ></div>
 
-          <div className="heading2">
+          <div
+            className="heading2  "
+            style={{
+              fontFamily: "Pixelify Sans",
+              fontSize: "20px",
+              lineHeight: "40px",
+              marginLeft: "35%",
+              textUnderlinePosition: "from-font",
+              textDecorationSkipInk: "none",
+              fontSize: "2.5vmax",
+            }}
+          >
             <h3>
               {" "}
               No sleep, all caffeine, and bugs are plot twists! <br /> Break,
@@ -20,46 +47,77 @@ const App = () => {
             </h3>
           </div>
         </div>
-<div className="foot mt-96">
-        <footer className="footer2  ">
-          <div className="flex my-20 justify-center  space-x-6 mb-6 text-blue-600">
-            <a href="https://www.instagram.com">
-              <img
-                className="social"
-                src="https://s3-alpha-sig.figma.com/img/da62/3df0/221483b4afbc2c7bfe87c3f21262c2b1?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UwRADuu~Aotw25Kc6eS57rKcHeIiPXg-LZywZgqMKTzrk03DzW0u-RLZJtZyHeeoVwbvgRfMB7~l4Zz2gPqGuZTFAXGb2DvaqFnIW8-jTrsaR2Ft0Vl2mffErbpvacNlMhlO87Y4axUWWA9aAqZq~wXQyHdboHSR08TYahUk7pJmo4Mp-o4R8oucKHjUvFYyK~tZPWBAEOolvclvPVUnjpJ47ZCq3iradaUeiSS0MvrfTvJxRJqx7IeseXlcura8nIiPtGT9oYCKWFQkgr1jqp3DLy~jRAi2hjE~w48uwcvQU4hbBmJ-w3JJKyRxqiqlWA4WMMgJpU3rUru7dT0A1A__"
-               
-              />
-            </a>
-            <a href="https://www.instagram.com">
-              <img
-                className="social"
-                src="https://s3-alpha-sig.figma.com/img/49a7/0aa7/c67706ed0b4b4c05a73755a76decdba1?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qO74KLJ3KEjniZdbTotC9I8-fUPT2PGJWs48PhSdE8vFcR4PWp24lREIANxjMZbu0T4lvl2FF1NwRbgytRysbN7n94BxM8EfggQhSwEcTGKKwukgvmiFRaQ5Q1rvezXAeWZ-0OcXW4oWD1dHEOmcdm9g0-9qd6C68pohISCmrwk1F178c2EhAQB-6VcLEqIywkxa62Jpf-3O0s87~ufp2Hz~onncrXUzo1bdluwQWted8I3YqzgTaAgmsUT0tKnr0~ZHsV2s0mAHB~3UoR5cXorxYXqRHg46hnlqiPa2YGW5-vrkZLO7VehQZpG~sQ4EUbb2KlVzUDgTE3qXAow03w__"
-              />
-            </a>
-            <a href="https://www.instagram.com">
-              <img
-                className="social"
-                src="https://s3-alpha-sig.figma.com/img/d85f/7d9c/8e11cce81b39d00f15df03b7e89ee6e9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UteIo03nRA6cG7WadYFWCowcuYNa6P7-SAQnN~W5FXdV5KVgdyeMp54MKQqx-aBQIbj28nHlTV6bmzKDaNvnAD5F1sWSKHX80jSoa2shllF0sjE6bNYnD74D6aze1RJbAT9er1EoDlAh-KywV~qLsWJ96sP0SSBTjn5qRtpa~85po2aAARbDDXdYVwSZf4ieZV4g2gMzOdYNv~kopd35VDrkCAQ72ms7A1uoqzZcnjoYUfPRgyTPhl01dRO65Ia8gfLDk8rCIg3hpLoNWmktmfynHmdHufHMU0JapDk5akh-eQaDDKQ1gbOKhAJ1OJgazuvGG5KH8-GZoHCiEJyesA__"
-              />
-            </a>
-            <a href="https://www.instagram.com">
-              <img
-                className="social"
-                src="https://s3-alpha-sig.figma.com/img/9bd7/9f3e/1d9901d56ed7cfe7e01e6ae12db367bb?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BgNS93srCNU7vP59jcLbh6TTZOdY94pjBSiM6jfg9w1R4lFDtKCwdMWqUSwHy65KXsh1qTOSrmo~J8ybeEx1m9itsTwACpuoM0IxE13okw4Gf~GGXlCwqmECTkqkCu1mucuejAwvmClZEWxn9SMk5gPYir~PDiNtiJ7CYfuIw~lCuMCjdg~A5I1cJw-lYZhmG9t0hj79Xv7kA~YBm5z5sfKcCZDB53b9gmyonBmntLdeaCzyMhcSO3SPwZuDKjYjkMl9hxxAFaXueCoYCoo2kCwkrjLTNL4hGMttEVXxRvapexKuIDk-lcyxoLtI0kpO3DfogRkuP5LHboIZWjA8Sg__"
-              />
-            </a>
-          </div>
+        <div className="foot mt-72">
+          <footer
+            className="footer2 "
+            style={{
+              fontSize: "xx-large",
 
-          <p>
-            Made with{" "}
-            <span role="img" aria-label="love">
-              ❤️
-            </span>{" "}
-            by GDG-GTBIT
-          </p>
-          <p>Copyright &copy;2025, All rights reserved.</p>
-        </footer>
-      </div>
+              width: "100%",
+              color: "black",
+            }}
+          >
+            <div className="flex my-20 justify-center  space-x-6 mb-3 text-blue-600">
+              <a href="https://www.instagram.com">
+                <img
+                  className="social "
+                  style={{
+                    width: "41px",
+                    height: "41px",
+                    gap: "0px",
+                    opacity: "0px",
+                  }}
+                  src="https://s3-alpha-sig.figma.com/img/da62/3df0/221483b4afbc2c7bfe87c3f21262c2b1?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UwRADuu~Aotw25Kc6eS57rKcHeIiPXg-LZywZgqMKTzrk03DzW0u-RLZJtZyHeeoVwbvgRfMB7~l4Zz2gPqGuZTFAXGb2DvaqFnIW8-jTrsaR2Ft0Vl2mffErbpvacNlMhlO87Y4axUWWA9aAqZq~wXQyHdboHSR08TYahUk7pJmo4Mp-o4R8oucKHjUvFYyK~tZPWBAEOolvclvPVUnjpJ47ZCq3iradaUeiSS0MvrfTvJxRJqx7IeseXlcura8nIiPtGT9oYCKWFQkgr1jqp3DLy~jRAi2hjE~w48uwcvQU4hbBmJ-w3JJKyRxqiqlWA4WMMgJpU3rUru7dT0A1A__"
+                />
+              </a>
+              <a href="https://www.instagram.com">
+                <img
+                  className="social"
+                  style={{
+                    width: "41px",
+                    height: "41px",
+                    gap: "0px",
+                    opacity: "0px",
+                  }}
+                  src="https://s3-alpha-sig.figma.com/img/49a7/0aa7/c67706ed0b4b4c05a73755a76decdba1?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qO74KLJ3KEjniZdbTotC9I8-fUPT2PGJWs48PhSdE8vFcR4PWp24lREIANxjMZbu0T4lvl2FF1NwRbgytRysbN7n94BxM8EfggQhSwEcTGKKwukgvmiFRaQ5Q1rvezXAeWZ-0OcXW4oWD1dHEOmcdm9g0-9qd6C68pohISCmrwk1F178c2EhAQB-6VcLEqIywkxa62Jpf-3O0s87~ufp2Hz~onncrXUzo1bdluwQWted8I3YqzgTaAgmsUT0tKnr0~ZHsV2s0mAHB~3UoR5cXorxYXqRHg46hnlqiPa2YGW5-vrkZLO7VehQZpG~sQ4EUbb2KlVzUDgTE3qXAow03w__"
+                />
+              </a>
+              <a href="https://www.instagram.com">
+                <img
+                  className="social"
+                  style={{
+                    width: "41px",
+                    height: "41px",
+                    gap: "0px",
+                    opacity: "0px",
+                  }}
+                  src="https://s3-alpha-sig.figma.com/img/d85f/7d9c/8e11cce81b39d00f15df03b7e89ee6e9?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UteIo03nRA6cG7WadYFWCowcuYNa6P7-SAQnN~W5FXdV5KVgdyeMp54MKQqx-aBQIbj28nHlTV6bmzKDaNvnAD5F1sWSKHX80jSoa2shllF0sjE6bNYnD74D6aze1RJbAT9er1EoDlAh-KywV~qLsWJ96sP0SSBTjn5qRtpa~85po2aAARbDDXdYVwSZf4ieZV4g2gMzOdYNv~kopd35VDrkCAQ72ms7A1uoqzZcnjoYUfPRgyTPhl01dRO65Ia8gfLDk8rCIg3hpLoNWmktmfynHmdHufHMU0JapDk5akh-eQaDDKQ1gbOKhAJ1OJgazuvGG5KH8-GZoHCiEJyesA__"
+                />
+              </a>
+              <a href="https://www.instagram.com">
+                <img
+                  className="social"
+                  style={{
+                    width: "41px",
+                    height: "41px",
+                    gap: "0px",
+                    opacity: "0px",
+                  }}
+                  src="https://s3-alpha-sig.figma.com/img/9bd7/9f3e/1d9901d56ed7cfe7e01e6ae12db367bb?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BgNS93srCNU7vP59jcLbh6TTZOdY94pjBSiM6jfg9w1R4lFDtKCwdMWqUSwHy65KXsh1qTOSrmo~J8ybeEx1m9itsTwACpuoM0IxE13okw4Gf~GGXlCwqmECTkqkCu1mucuejAwvmClZEWxn9SMk5gPYir~PDiNtiJ7CYfuIw~lCuMCjdg~A5I1cJw-lYZhmG9t0hj79Xv7kA~YBm5z5sfKcCZDB53b9gmyonBmntLdeaCzyMhcSO3SPwZuDKjYjkMl9hxxAFaXueCoYCoo2kCwkrjLTNL4hGMttEVXxRvapexKuIDk-lcyxoLtI0kpO3DfogRkuP5LHboIZWjA8Sg__"
+                />
+              </a>
+            </div>
+
+            <p>
+              Made with{" "}
+              <span role="img" aria-label="love">
+                ❤️
+              </span>{" "}
+              by GDG-GTBIT
+            </p>
+            <p>Copyright &copy;2025, All rights reserved.</p>
+          </footer>
+        </div>
       </div>
     </React.Fragment>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,114 @@
-
-      // Import necessary modules
 import React from "react";
-import "./style.css"; // Ensure to create corresponding CSS
 
 const Header = () => {
   return (
-    <div className="app  min-h-screen
-    ">
+    <div
+      className="app py-10 min-h-screen text-center text-gray-900"
+      style={{  minHeight: "100vh", textAlign: "center", color: "#1f2937" }}
+    >
       {/* Top Section */}
-     
-        <div className="circle">
-          
-        </div>
-        <div className="heading"><h1>Sign up to receive updates for <br />the latest event announcements</h1></div>
+      <div
+        className="circle"
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: "#ccc",
+          borderRadius: "50%",
+          margin: "auto",
+          marginBottom: "0%",
+          marginBlock: "1.5vh",
+        }}
+      ></div>
+      <div
+        className="heading"
+        style={{
+          fontWeight: "bolder",
+          fontSize: "4vmax",
+          margin: "auto",
+        }}
+      >
+        <h1>
+          Sign up to receive updates for <br />
+          the latest event announcements
+        </h1>
+      </div>
 
-
-        <div className="email-input-container ">
-      <input
-        type="email"
-        placeholder="Enter your e-mail address"
-        className="email-input"
-      />
-      <button className="email-submit-button ">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          fill="#5a67d8"
+      <div
+        className="email-input-container flex items-center mt-4"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#ffffff",
+          borderRadius: "50px",
+          padding: "15px 25px 10px",
+          boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+          maxWidth: "400px",
+          margin: "auto",
+          marginTop: "7.6vh",
+        }}
+      >
+        <input
+          type="email"
+          placeholder="Enter your e-mail address"
+          className="email-input p-2 border border-gray-300 rounded-l-md focus:outline-none"
+          style={{
+            flexGrow: 1,
+            border: "none",
+            outline: "none",
+            background: "transparent",
+            fontSize: "16px",
+            color: "#000000",
+          }}
+        />
+        <button
+          className="email-submit-button p-2  text-white rounded-r-md"
+          style={{
+            backgroundColor: "#e5efff",
+            border: "none",
+            outline: "none",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "8px",
+            cursor: "pointer",
+            borderRadius: "50%",
+            transition: "background-color 0.3s ease",
+          }}
+          onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#869dbd")}
+          onMouseOut={(e) => (e.currentTarget.style.backgroundColor = "#e5efff")}
         >
-          <path d="M10 17l5-5-5-5v10z" />
-        </svg>
-      </button>
-    </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            fill="currentColor"
+          >
+            <path d="M10 17l5-5-5-5v10z" />
+          </svg>
+        </button>
+      </div>
 
-        
       {/* Footer Section */}
-      <footer className="footer">
-        <p>Made with <span role="img" aria-label="love">❤️</span> by GDG-GTBIT</p>
+      <footer
+        className="footer mt-10 text-center"
+        style={{
+          fontSize: "xx-large",
+          background: "#7c9acb",
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          color: "black",
+          borderRadius: "30px",
+        }}
+      >
+        <p>
+          Made with <span role="img" aria-label="love">❤️</span> by GDG-GTBIT
+        </p>
         <p>Copyright &copy;2025, All rights reserved.</p>
       </footer>
-    
-
-    
     </div>
   );
 };
 
-
-export default Header
+export default Header;
